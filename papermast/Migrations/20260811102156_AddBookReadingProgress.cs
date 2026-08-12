f0008935643a768d294b3b6ff23a1ab9f8f1c521
Brad <35670553+Grimshawb@users.noreply.github.com>
2026-08-12 17:47:44 -0400

    Add book reading progress migration. Add EF to tools

---
+++ papermast/Migrations/20260811102156_AddBookReadingProgress.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace papermast.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBookReadingProgress : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PagesCompleted",
+                table: "BookEntries",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PercentCompleted",
+                table: "BookEntries",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "BookEntries",
+                type: "varchar(50)",
+                maxLength: 50,
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PagesCompleted",
+                table: "BookEntries");
+
+            migrationBuilder.DropColumn(
+                name: "PercentCompleted",
+                table: "BookEntries");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "BookEntries");
+        }
+    }
+}
