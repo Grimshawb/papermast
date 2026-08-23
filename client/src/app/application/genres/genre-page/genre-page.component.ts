90e96e0bef733b02c83ff7cf876001953dc5e128
Brad <35670553+Grimshawb@users.noreply.github.com>
2026-08-23 15:00:20 -0400

    Updated all genres to curated

--- client/src/app/application/genres/genre-page/genre-page.component.ts
+++ client/src/app/application/genres/genre-page/genre-page.component.ts
@@ -2,9 +2,9 @@ import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 import { DatePipe } from '@angular/common';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { ActivatedRoute, RouterLink } from '@angular/router';
-import { catchError, defaultIfEmpty, finalize, map, of, switchMap } from 'rxjs';
-import { ApiBook, CuratedCatalogSection, Genre, GENRES } from '../../../models';
-import { BooksApiService, CuratedCatalogService } from '../../../services';
+import { catchError, finalize, map, of, switchMap } from 'rxjs';
+import { CuratedCatalogSection, Genre, GENRES } from '../../../models';
+import { CuratedCatalogService } from '../../../services';
 import { BookListEntryComponent } from '../../home/components/book-list-entry/book-list-entry.component';
 
 @Component({
@@ -16,16 +16,13 @@ import { BookListEntryComponent } from '../../home/components/book-list-entry/bo
 })
 export class GenrePageComponent implements OnInit {
   public genre: Genre | undefined;
-  public books: ApiBook[] = [];
   public curatedSections: CuratedCatalogSection[] = [];
-  public isCurated = false;
   public isLoading = true;
   public loadFailed = false;
   private readonly destroyRef = inject(DestroyRef);
 
   constructor(
     private route: ActivatedRoute,
-    private booksApiService: BooksApiService,
     private curatedCatalogService: CuratedCatalogService
   ) {}
 
@@ -34,38 +31,18 @@ export class GenrePageComponent implements OnInit {
       switchMap(params => {
         const slug = params.get('slug') || '';
         this.genre = GENRES.find(item => item.slug === slug);
-        this.books = [];
         this.curatedSections = [];
-        this.isCurated = false;
         this.loadFailed = false;
         this.isLoading = !!this.genre;
 
-        if (!this.genre) return of([] as ApiBook[]);
+        if (!this.genre) return of([] as CuratedCatalogSection[]);
         return this.curatedCatalogService.get(this.genre.slug).pipe(
-          defaultIfEmpty(null),
-          switchMap(catalog => {
-            if (catalog?.publishedAt || this.genre?.slug === 'horror') {
-              this.isCurated = true;
-              this.curatedSections = catalog?.sections ?? [];
-              return of([] as ApiBook[]);
-            }
-
-            return this.booksApiService.genre(this.genre!.slug).pipe(
-              defaultIfEmpty(null),
-              map(books => {
-                if (books === null) {
-                  this.loadFailed = true;
-                  return [] as ApiBook[];
-                }
-                return books;
-              })
-            );
-          }),
-          catchError(() => { this.loadFailed = true; return of([] as ApiBook[]); }),
+          map(catalog => catalog.sections ?? []),
+          catchError(() => { this.loadFailed = true; return of([] as CuratedCatalogSection[]); }),
           finalize(() => this.isLoading = false)
         );
       }),
       takeUntilDestroyed(this.destroyRef)
-    ).subscribe(books => this.books = books);
+    ).subscribe(sections => this.curatedSections = sections);
   }
 }
